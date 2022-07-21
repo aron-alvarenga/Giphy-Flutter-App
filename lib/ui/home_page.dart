3a87a74c6961d8b43300accb0079ab2ea529ae95
aron-alvarenga <aron.alvarenga@hotmail.com>
2022-07-21 01:13:20 -0400

    GIF page created

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:giphy_app/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
@@ -129,6 +130,14 @@ class _HomePageState extends State<HomePage> {
               height: 300.0,
               fit: BoxFit.cover,
             ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GifPage(snapshot.data['data'][index]),
+                ),
+              );
+            },
           );
         } else {
           return GestureDetector(
