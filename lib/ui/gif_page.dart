41805865c57ae49b08eb8c7213453ae92d8b1ca6
aron-alvarenga <aron.alvarenga@hotmail.com>
2022-07-21 17:31:36 -0400

    Feature Share GIF added

--- lib/ui/gif_page.dart
+++ lib/ui/gif_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
   final Map _gifData;
@@ -12,6 +15,14 @@ class GifPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_gifData['title']),
         backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.share),
+            onPressed: () {
+              Share.share(_gifData['images']['fixed_height']['url']);
+            },
+          ),
+        ],
       ),
       backgroundColor: Colors.black,
       body: Center(
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:giphy_app/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -136,6 +137,11 @@ class _HomePageState extends State<HomePage> {
                 MaterialPageRoute(
                   builder: (context) => GifPage(snapshot.data['data'][index]),
                 ),
+              );
+            },
+            onLongPress: () {
+              Share.share(
+                snapshot.data['data'][index]['images']['fixed_height']['url'],
               );
             },
           );
